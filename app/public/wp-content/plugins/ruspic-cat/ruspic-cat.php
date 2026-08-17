fc0e8e7512c8384a2e9c744a660cc613a9db0b5c
semakininfo-tech <semakin.info@gmail.com>
2026-08-17 20:35:23 +0300

    RUSPIC Cat 1.1.0: bump plugin version for refined product-card UI

--- app/public/wp-content/plugins/ruspic-cat/ruspic-cat.php
+++ app/public/wp-content/plugins/ruspic-cat/ruspic-cat.php
@@ -3,7 +3,7 @@
  * Plugin Name: RUSPIC Cat
  * Plugin URI: https://github.com/semakininfo-tech/ruspic-main
  * Description: Независимый каталог RUSPIC с брендами, категориями, товарами, характеристиками, REST API и собственной корзиной-заявкой. Не требует WooCommerce.
- * Version: 1.0.0
+ * Version: 1.1.0
  * Author: RUSPIC
  * Requires at least: 6.6
  * Requires PHP: 8.1
@@ -12,7 +12,7 @@
 
 if ( ! defined( 'ABSPATH' ) ) { exit; }
 
-define( 'RUSPIC_CAT_VERSION', '1.0.0' );
+define( 'RUSPIC_CAT_VERSION', '1.1.0' );
 define( 'RUSPIC_CAT_FILE', __FILE__ );
 define( 'RUSPIC_CAT_DIR', plugin_dir_path( __FILE__ ) );
 define( 'RUSPIC_CAT_URL', plugin_dir_url( __FILE__ ) );
